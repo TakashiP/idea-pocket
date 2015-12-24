c512f49fbd11f00a7e149cf44dc2933ffa1ab779
TakashiP <masai.takashi@gmail.com>
2015-12-24 08:26:32 +0000

    set user and pocket

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,6 @@
 class StaticPagesController < ApplicationController
+before_action :set_user, :set_pocket
+
   def home
     @user = current_user if logged_in?
     @pocket = current_user.pockets.build if logged_in?
@@ -6,4 +8,15 @@ class StaticPagesController < ApplicationController
     @pockets = @user.pockets.order(created_at: :desc) if logged_in?
     @ideas = @user.ideas.order(created_at: :desc) if logged_in?
   end
+  
+  private
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
+  def set_pocket
+    @pocket = Pocket.find(params[:id])
+  end
+  
 end
